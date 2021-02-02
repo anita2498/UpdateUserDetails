373de3b4c52a7290a12fb3a1d13f0175ab3ee20a
anita2498 <anitaputhenpurayil@gmail.com>
2021-02-01 23:17:11 -0500

    Logger added

--- src/main/java/com/hcl/phase3_1/Controller/UserController.java
+++ src/main/java/com/hcl/phase3_1/Controller/UserController.java
@@ -1,7 +1,8 @@
 package com.hcl.phase3_1.Controller;
 
 import java.util.Optional;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.hcl.phase3_1.Repo.UserEntity;
 import com.hcl.phase3_1.Services.Services;
 
+
 @Controller
 @RequestMapping("user")
 public class UserController {
@@ -22,14 +24,16 @@ public class UserController {
 	Services services;
 	@Autowired
 	Optional<UserEntity> user;
-	
+	Logger logger = LoggerFactory.getLogger(UserController.class);
 	@GetMapping(path = "/")
 	String hello() {
+		logger.info("Mapping to index");
 		return "user/index";
 	}
 	
 	@PostMapping (path="/find")
 	String find(@RequestParam (name="userid") String userid , Model model) {
+		logger.info("post mapping /find");
 		user = services.findById(userid);
 		if(user.isPresent()) {
 			System.out.println("here"+ user.toString());
@@ -49,6 +53,7 @@ public class UserController {
 	
 	@PostMapping(path = "/update")
 	String update(@ModelAttribute("user") UserEntity user, ModelMap model) {
+		logger.info("in /update");
 		System.out.println("here update" + user.toString());
 		services.update(user);
 		model.addAttribute("msg", "<div style=\"border:1px dotted red;padding:2%;\">" +"User Information Updated");
